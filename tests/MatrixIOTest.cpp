0e1b71160e581879020819582fc94a25ff0ce40c
sashansamarajeewa <sashan1995@gmail.com>
2025-02-02 16:56:13 +0100

    Fix format MatrixIOTest.cpp

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,4 +1,5 @@
 #define BOOST_TEST_DYN_LINK
+#include <Eigen/Dense>
 #include "matrixIO.hpp"
 #include <boost/test/unit_test.hpp>
 
@@ -6,7 +7,8 @@ using namespace Eigen;
 
 BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 
-BOOST_AUTO_TEST_CASE(openDataTest) {
+BOOST_AUTO_TEST_CASE(openDataTest)
+{
   MatrixXd expectedMatrix(3, 3);
   expectedMatrix << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459,
       0.566198, -0.604897, -0.444451;
