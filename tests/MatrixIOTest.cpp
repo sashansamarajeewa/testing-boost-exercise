78360103c4427ab90e447fdbfb22a0b2d2148536
sashansamarajeewa <sashan1995@gmail.com>
2025-02-02 16:50:32 +0100

    Fix format MatrixIOTest.cpp

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,21 +1,19 @@
 #define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
 #include "matrixIO.hpp"
+#include <boost/test/unit_test.hpp>
 
 using namespace Eigen;
 
 BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 
-BOOST_AUTO_TEST_CASE(openDataTest)
-{
-    MatrixXd expectedMatrix(3, 3);
-    expectedMatrix << 0.680375, 0.59688, -0.329554,
-      -0.211234, 0.823295, 0.536459,
+BOOST_AUTO_TEST_CASE(openDataTest) {
+  MatrixXd expectedMatrix(3, 3);
+  expectedMatrix << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459,
       0.566198, -0.604897, -0.444451;
 
-    MatrixXd actualMatrix = matrixIO::openData("../data/m3.csv", 3);
+  MatrixXd actualMatrix = matrixIO::openData("../data/m3.csv", 3);
 
-    BOOST_CHECK(actualMatrix.isApprox(expectedMatrix));
+  BOOST_CHECK(actualMatrix.isApprox(expectedMatrix));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
